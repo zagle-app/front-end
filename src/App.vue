ad90620b047685268ed18ff9170d95df16de341e
EmpioS <pawel.wlo@wp.pl>
2020-10-13 20:00:19 +0200

    Dodanie nawigacji i routingu

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<template>
+  <div class="ma-12 pa-12">
+    <template>
+        <v-navigation-drawer
+            permanent
+            expand-on-hover
+            absolute
+        >
+          <v-list>
+            <v-list-item>
+                <v-img contain max-height="5em" src="./assets/logomini.png"></v-img>
+            </v-list-item>
+          </v-list>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+          <v-divider></v-divider>
+
+          <v-list
+              nav
+              dense
+          >
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-folder</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title><router-link to="/">Home</router-link></v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-account-multiple</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title><router-link to="/About">About</router-link></v-list-item-title>
+            </v-list-item>
+          </v-list>
+
+        </v-navigation-drawer>
+      <v-container>
+        <router-view/>
+      </v-container>
+    </template>
+  </div>
+</template>
+
+<style lang="scss">
 </style>
