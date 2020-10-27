0c7fd41bf73f59fb54e82bfd09dd0e4e75da0848
Empios <pawelwlodarczyk97@yahoo.com>
2020-10-27 17:10:19 +0100

    Dodanie logowanie google

---
+++ src/components/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <GoogleLogin :params="params" :renderParams="renderParams" :onSuccess="onSuccess"></GoogleLogin>
+</template>
+
+<script>
+import GoogleLogin from 'vue-google-login';
+export default {
+  name: 'login',
+  data() {
+    return {
+      params: {
+        client_id: "637993854274-8lo6ukh579ed0b4i0jjikqqppiqa23qj.apps.googleusercontent.com"
+      },
+      renderParams: {
+        width: 250,
+        height: 50,
+        longtitle: true
+      }
+    }
+  },
+  components: {
+    GoogleLogin
+  },
+  methods: {
+    onSuccess(googleUser) {
+      console.log(googleUser);
+    }
+  }
+}
+  </script>
